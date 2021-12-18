cbfcde54490337434966efcb8ac400ef236090df
LêMinhThành <68367028+minhthanh120@users.noreply.github.com>
2021-12-18 19:22:46 +0700

    first commit

---
+++ OOP-sort.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+class array
+{
+private:
+    /* data */
+    int n;
+    int arr[n];
+public:
+    array(/* args */);
+    ~array();
+    input(){
+        
+    };
+};
+
+
+array::array(/* args */)
+{
+}
+
+array::~array()
+{
+}
